46423d2d462a042db07324ed8fff60ec8b571e47
Stephen Crocker <crok001@yahoo.co.uk>
2021-01-23 14:35:27 +0000

    Sing tool actually works (mostly).  Seems to crash ehwn cycling through the motor functions.

--- chatbird-tools/sing.c
+++ chatbird-tools/sing.c
@@ -13,9 +13,10 @@ int fp;
 
 int fillsine(short *buf, float pitch)
 {
-  for (unsigned i = 0; i < 22; i++)
+  printf("Pitch=%f\n",pitch);
+  for (unsigned i = 0; i < 22000; i++)
     {
-      buf[i] = (short)(256*sin((i * pitch * M_PI ) / 160.0));
+      buf[i] = (short)(16383*sin((i * pitch * M_PI ) / 44000));
     }
 
 }
@@ -23,7 +24,7 @@ int fillsine(short *buf, float pitch)
 int main(int argc, char *argv[])
 {
   int i;
-  short buf[22];
+  short buf[22000];
   char *szDev="/dev/chatbird0";
   
   for(i=0;i<argc;i++)
@@ -38,34 +39,38 @@ int main(int argc, char *argv[])
 
   fp=open(szDev,O_RDWR);
   int tmp;
-  float pitch=1;
+  float pitch=1000;
   int cmd=0;
   fillsine(buf, pitch);
+  i=0;
   do
     {
       ioctl(fp,CHATBIRD_IOCGETBUTTONS,&tmp);
       if(tmp&2)
 	{
-	  pitch-=0.1;
+	  pitch-=10;
 	  fillsine(buf, pitch);
 	}
       if(tmp&4)
 	{
-	  pitch+=0.1;
+	  pitch+=10;
 	  fillsine(buf, pitch);
 	}
       if(tmp&1)
 	{
 	  cmd++;
 	  cmd%=4;
-	  int val=0x01008000+(cmd+2);
+	  int val=0x10008000+(cmd+2);
 	  ioctl(fp,CHATBIRD_IOCSETMOTOR,&val);
 	}
-      write(fp,buf,44);
-      //usleep(5000);
+
+      write(fp,buf+i,44);
+      i+=22;
+      i%=22000;
+
+      usleep(500);
       //pitch+=0.1;
       //fillsine(buf, pitch);
-      usleep(1000);
     } while((tmp&8)==0);
   close(fp);
 }
